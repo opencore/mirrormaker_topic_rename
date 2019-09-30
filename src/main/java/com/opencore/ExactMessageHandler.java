7521d2cfcd9784e23951eb251c156e906dd6156b
Sönke Liebau <soenke.liebau@opencore.com>
2019-09-30 11:53:49 +0200

    Added ExactMessageHandler class that implements copying of partition information to bypass re-partititoning of data, hence avoiding potential partition mixups for data with no keys.

---
+++ src/main/java/com/opencore/ExactMessageHandler.java
@@ -0,0 +1,22 @@
+package com.opencore;
+
+import java.util.Collections;
+import java.util.List;
+import kafka.consumer.BaseConsumerRecord;
+import kafka.tools.MirrorMaker;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * An implementation of MirrorMakerMessageHandler that can be used when a topic should be recreated as close
+ * to identical as possible.
+ * This will preserve topic, partition, timestamp, headers, key and value.
+ *
+ * Note: by preserving the exact partition this handler bypasses the partitioning algorithm, which means that the
+ * target topic has to be set up with at least as many partitions as the source topic, as otherwise messages will
+ * be written to non-existent partitions which will fail.
+ */
+public class ExactMessageHandler implements MirrorMaker.MirrorMakerMessageHandler {
+  public List<ProducerRecord<byte[], byte[]>> handle(BaseConsumerRecord record) {
+    return Collections.singletonList(new ProducerRecord<byte[], byte[]>(record.topic(), record.partition(), record.timestamp(), record.key(), record.value(), record.headers()));
+  }
+}
