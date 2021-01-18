9870703c88648aec0b4f550f3ae6e4a060d8b1f9
Kaloyan Dimitrov <1196858+kalski@users.noreply.github.com>
2021-01-18 16:56:16 +0200

    * Added optional topics CSV argument to ExactMessageHandler filtering messages to be exactly copied * Fixed typos in Readme.md

--- src/main/java/com/opencore/ExactMessageHandler.java
+++ src/main/java/com/opencore/ExactMessageHandler.java
@@ -1,9 +1,12 @@
 package com.opencore;
 
+import java.util.Collection;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import kafka.consumer.BaseConsumerRecord;
 import kafka.tools.MirrorMaker;
+import kafka.tools.MirrorMaker.defaultMirrorMakerMessageHandler$;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.record.RecordBatch;
 
@@ -17,8 +20,38 @@ import org.apache.kafka.common.record.RecordBatch;
  * be written to non-existent partitions which will fail.
  */
 public class ExactMessageHandler implements MirrorMaker.MirrorMakerMessageHandler {
+  private Collection<String> topics;
+
+  /**
+   * Constructor that will be called when no args are passed on the command line
+   */
+  public ExactMessageHandler() {
+
+  }
+
+  /**
+   * Constructor that will be passed the args passed on the command line.
+   *
+   * @param topicsArg CSV of topics for which messages will be exactly recreated with this handler.
+   */
+  public ExactMessageHandler(String topicsArg) {
+    this.topics = new HashSet<>();
+
+    String[] topicsArgArray = topicsArg.split(",");
+    for (String topicArg: topicsArgArray ) {
+      topics.add(topicArg);
+    }
+  }
+
   public List<ProducerRecord<byte[], byte[]>> handle(BaseConsumerRecord record) {
-    Long timestamp = record.timestamp() == RecordBatch.NO_TIMESTAMP ? null : record.timestamp();
-    return Collections.singletonList(new ProducerRecord<byte[], byte[]>(record.topic(), record.partition(), timestamp, record.key(), record.value(), record.headers()));
+    if (topics == null || topics.contains(record.topic())) {
+
+      Long timestamp = record.timestamp() == RecordBatch.NO_TIMESTAMP ? null : record.timestamp();
+      return Collections.singletonList(
+          new ProducerRecord<byte[], byte[]>(record.topic(), record.partition(), timestamp, record.key(), record.value(),
+              record.headers()));
+    } else {
+      return defaultMirrorMakerMessageHandler$.MODULE$.handle(record);
+    }
   }
 }
